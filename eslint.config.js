fc52232db3d87ad62e2dff5844540c82397909b8
Iman Moazzen <imanmoaz@gmail.com>
2025-01-20 17:49:07 -0800

    added the yaml file and eslint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Enables browser globals (window, document, etc.)
+        ...globals.node, // Enables Node.js globals (process, __dirname, etc.)
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detects React version
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
